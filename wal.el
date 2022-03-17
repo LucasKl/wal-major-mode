dceceb60b22a143a8919ea59d268646067b0f3c5
Lucas Klemmer <lucas.klemmer@jku.at>
2022-03-17 16:12:06 +0100

    comint integration

--- wal.el
+++ wal.el
@@ -45,12 +45,6 @@
 	;; Color numbers
 	("-?[[:digit:]]+" . font-lock-constant-face)))
 
-(define-derived-mode wal-mode prog-mode "WAL Mode"
-  :syntax-table wal-mode-syntax-table
-  (setq font-lock-defaults '(wal-highlights))
-  (font-lock-fontify-buffer)
-  (local-set-key (kbd "M-<tab>") 'wal-complete-symbol))
-
 (add-to-list 'auto-mode-alist '("\\.wal\\'" . wal-mode))
 
 ;; Completion
@@ -63,7 +57,7 @@
 	"&&" "call" "for" "last" "resolve-group"
 	"unalias" "*" "case" "get" "length"
 	"resolve-scope" "unless" "**" "cond" "geta"
-	"let" "rest" "unload" "+" "convert/bin     geta/default"
+	"let" "rest" "unload" "+" "convert/bin" "geta/default"
 	"letret" "reval" "unset-scope" "-" "count"
 	"groups" "list" "second" "when" "/"
 	"defun" "help" "list?" "set" "whenever"
@@ -79,7 +73,8 @@
 
 (defun wal-complete-symbol ()
   "Perform keyword completion on current symbol.
-This uses `ido-mode' user interface for completion."
+This uses `ido-mode' user interface for completion.
+Source: http://xahlee.info/emacs/emacs/elisp_keyword_completion.html"
   (interactive)
   (let* (
          ($bds (bounds-of-thing-at-point 'symbol))
@@ -95,3 +90,93 @@ This uses `ido-mode' user interface for completion."
           (ido-completing-read "" wal-keywords nil nil $current-sym ))
     (delete-region $p1 $p2)
     (insert $result-sym)))
+
+;; ------------------------------ WAL eval ------------------------------
+(defun wal-eval-sexpr-behind ()
+  (interactive)
+  (setq end (point))
+  (backward-sexp)
+  (setq start (point))
+  (goto-char end)
+  (setq sexpr (replace-regexp-in-string "\n" " " (buffer-substring start end)))
+  (wal-eval-sexpr sexpr))
+
+(defun wal-eval-sexpr-forward ()
+  (interactive)
+  (setq start (point))
+  (forward-sexp)
+  (setq end (point))
+  (goto-char start)
+  (setq sexpr (replace-regexp-in-string "\n" " " (buffer-substring start end)))
+  (wal-eval-sexpr sexpr))
+
+(defun wal-eval-buffer ()
+  (interactive)
+  (setq buffer-content (buffer-substring-no-properties 1 (buffer-size)))
+  ;; remove comments and blank lines
+  (setq sexpr (replace-regexp-in-string ";.*\n\\|^[[:space:]]+$" "" buffer-content))
+  ;; remove line breaks
+  (setq sexpr (replace-regexp-in-string "\n" " " sexpr))
+  ;; remove unnecessary whitespace
+  (setq sexpr (replace-regexp-in-string "[[:space:]]+" " " sexpr))
+  ;; wrap everything in a do function
+  (setq sexpr (concat "(do " sexpr ")"))
+  (wal-eval-sexpr sexpr))
+
+(defun wal-eval-sexpr (sexpr)
+  (with-current-buffer (get-buffer "*WAL*")  
+    (comint-send-string "*WAL*" sexpr)
+    (comint-send-input)))
+
+;; Create Major Mode
+(define-derived-mode wal-mode scheme-mode "WAL Mode"
+  :syntax-table wal-mode-syntax-table
+  (setq font-lock-defaults '(wal-highlights))
+  (font-lock-fontify-buffer)
+  (local-set-key (kbd "M-<tab>") 'wal-complete-symbol)
+  (local-set-key (kbd "C-x C-e") 'wal-eval-sexpr-behind)
+  (local-set-key (kbd "C-M-x") 'wal-eval-sexpr-forward))
+
+
+(defvar wal-prompt-regexp "^>->" "WAL Prompt >->.")
+
+(defvar wal-cli-arguments '()
+  "Commandline arguments to pass to `wal-cli'")
+
+(defvar wal-mode-map
+  (let ((map (nconc (make-sparse-keymap) comint-mode-map)))
+    ;; example definition
+    (define-key map "\t" 'completion-at-point)
+    map)
+  "Basic mode map for `run-wal'")
+
+(defun wal-repl-initialize ()
+  "Helper function to initialize WAL"
+  (setq comint-process-echoes t)
+  (setq comint-use-prompt-regexp t))
+
+(defun run-wal ()
+  (interactive)
+  (setq wal-repl-buffer (get-buffer-create "*WAL*"))
+  (apply 'make-comint-in-buffer "Wal" wal-repl-buffer
+         "wal" '())
+  (display-buffer "*WAL*"))
+
+(define-derived-mode wal-repl-mode comint-mode "Wal"
+  "Major mode for `run-wal'.
+
+\\<wal-mode-map>"
+  nil "Wal"
+  ;; this sets up the prompt so it matches things like: [foo@bar]
+  (setq comint-prompt-regexp wal-prompt-regexp)
+  ;; this makes it read only; a contentious subject as some prefer the
+  ;; buffer to be overwritable.
+  (setq comint-prompt-read-only t)
+  ;; this makes it so commands like M-{ and M-} work.
+  (set (make-local-variable 'paragraph-separate) "\\'")
+  ;(set (make-local-variable 'font-lock-defaults) '(wal-font-lock-keywords t))
+  (set (make-local-variable 'paragraph-start) wal-prompt-regexp))
+
+(add-hook 'wal-repl-mode-hook 'wal-repl-initialize)
+;(add-to-list 'auto-mode-alist '("*WAL*" . wal-repl-mode))
+

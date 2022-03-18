63ab0b447f4c2e1a531b38983864fd739989e170
Lucas Klemmer <lucas.klemmer@jku.at>
2022-03-18 12:32:37 +0100

    package header

--- wal.el
+++ wal.el
@@ -2,13 +2,51 @@
 
 ;; Copyright (C) 2022  Lucas Klemmer
 
-;; Author: Lucas Klemmer
+;; Author: Lucas Klemmer <lucas.klemmer@jku.at>
 ;; Keywords: languages
+;; URL: https://github.com/LucasKl/wal-major-mode
+
+;; Version: 1.0
+
+;;; License:
+
+;; BSD 3-Clause License
+
+;; Copyright (c) 2022, Lucas Klemmer
+;; All rights reserved.
+
+;; Redistribution and use in source and binary forms, with or without
+;; modification, are permitted provided that the following conditions are met:
+
+;; 1. Redistributions of source code must retain the above copyright notice, this
+;;    list of conditions and the following disclaimer.
+
+;; 2. Redistributions in binary form must reproduce the above copyright notice,
+;;    this list of conditions and the following disclaimer in the documentation
+;;    and/or other materials provided with the distribution.
+
+;; 3. Neither the name of the copyright holder nor the names of its
+;;    contributors may be used to endorse or promote products derived from
+;;    this software without specific prior written permission.
+
+;; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+;; AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+;; IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+;; DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+;; FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+;; DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+;; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+;; CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+;; OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+;; OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ;;; Commentary:
 
 ;;; Code:
 
+(require 'ido)
+
+;; ------------------------------ Highlighting ------------------------------
 (defconst wal-mode-syntax-table
   (let ((table (make-syntax-table)))
     ;; ; is a comment starter
@@ -56,10 +94,7 @@
 
 (add-to-list 'auto-mode-alist '("\\.wal\\'" . wal-mode))
 
-;; Completion
-(require 'ido) ; part of emacs
-
-;; this is your lang's keywords
+;; ------------------------------ Completion ------------------------------
 (setq wal-keywords
       '("!" "atom?" "fold" "int?" "range" "symbol?" "!="
 	"average" "fold/signal" "lambda" "require" "type"
